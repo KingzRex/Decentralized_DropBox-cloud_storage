abb52f2814771a9bbfc73fd19601f4b61580394d
KingzRex <67268931+KingzRex@users.noreply.github.com>
2020-11-15 01:47:51 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ class Navbar extends Component {
           rel="noopener noreferrer"
         >
           <img src={box} width="30" height="30" className="align-top" alt="" />
-          D$t0r@g3
+         Too's D$t0r@g3
         </a>
         <ul className="navbar-nav px-3">
           <li>
